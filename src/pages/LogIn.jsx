1e8bd9c9292319b38d4fde27c8c6f9c9f5001766
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-03-27 12:23:55 +0800

    feat: enable sign in sign up for review

---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Home from "./Home";
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import Layout from '../layouts/Layout';
+
+
+const LogIn = () => {
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+      <div className="App">
+        <div>Hello, {user.username}</div>
+        <Home />
+          <AmplifySignOut />
+      </div>
+    ) : (
+      <AmplifyAuthenticator>
+        <AmplifySignUp
+          slot="sign-up"
+          formFields={[
+            { type: "username" },
+            { type: "password" },
+            { type: "email" }
+          ]}
+        />
+      </AmplifyAuthenticator>
+  );
+}
+
+export default LogIn;
