d0019a106a6da8b8e108161788bfbabd0f9d1650
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-04-03 03:10:28 +0800

    feat: add back to home button in sign in

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import Layout from '../layouts/Layout';
+import { Button } from 'bootstrap';
 
 
 const LogIn = () => {
@@ -20,7 +21,11 @@ const LogIn = () => {
       <div className="App">
         <Home />
       </div>
-    ) : (
+  ) : (
+      <div>
+      <Link to="/" className="btn btn-primary">
+        Back to Home
+       </Link>
       <AmplifyAuthenticator>
         <AmplifySignUp
           slot="sign-up"
@@ -30,7 +35,8 @@ const LogIn = () => {
             { type: "email" }
           ]}
         />
-      </AmplifyAuthenticator>
+        </AmplifyAuthenticator>
+      </div>
   );
 }
 
