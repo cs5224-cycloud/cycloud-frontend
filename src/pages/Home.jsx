dc42f0335df9e416a00f74d9b41cc58c1d3afadd
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-04-07 20:41:53 +0800

    feat: move criteria slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,6 +70,64 @@ const Home = () => {
   const history = useHistory();
   const navigateToLogin = () => history.push("/login"); //eg.history.push('/login');
 
+  const [difficulty, setDifficulty] = useState(50);
+  const [views, setViews] = useState(50);
+  const [traffic, setTraffic] = useState(50);
+
+  const handleDifficultyChange = ({ target: { value } }) =>
+    setDifficulty(value);
+
+  const handleViewsChange = ({ target: { value } }) => setViews(value);
+
+  const handleTrafficChange = ({ target: { value } }) => setTraffic(value);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    console.log(form);
+    console.log(difficulty, views, traffic);
+    console.log(JSON.stringify({
+      inputs: [
+        { numberValue: difficulty },
+        { numberValue: views },
+        { numberValue: traffic }
+      ]
+    }));
+  };
+
+  const submitSearch = () => {
+    fetch(
+      "https://us-central1-sage-philosophy-309216.cloudfunctions.net/demo_reco_endpoint",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          inputs: [
+            { numberValue: difficulty },
+            { numberValue: views },
+            { numberValue: traffic }
+          ]
+        }),
+      }
+    )
+      .then((data) => data.json())
+      .then(json => {
+        var parsedData = json.payload;
+        var maximumScore = 0;
+        var bestRoute = -1;
+        for (var i = 0; i < parsedData.length; i++) {
+          if (parsedData[i].tables.score > maximumScore) {
+            maximumScore = parsedData[i].tables.score;
+            bestRoute = parseInt(parsedData[i].tables.stringValue);
+          }
+        }
+        console.log(bestRoute);
+        handleChoseRoute(bestRoute);
+      });
+  }
+
   return (
     <>
       {isLoggedIn ? <h1>Hi {username}</h1> : <h1></h1>}
@@ -100,7 +158,55 @@ const Home = () => {
                 <Tabs defaultActiveKey="search" id="uncontrolled-tab">
                   <Tab eventKey="criteria" title="Search by Criteria">
                     <br />
-                    <CriteriaSliders submitHandler={handleChoseRoute}/>
+
+                          <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="difficultyRange">
+          <Form.Label>Route Difficulty</Form.Label>
+          <Row>
+            <Col md={LABEL_WIDTH}>Easy</Col>
+            <Col md={SLIDER_WIDTH}>
+              <Form.Control
+                type="range"
+                onChange={handleDifficultyChange}
+                value={difficulty}
+              />
+            </Col>
+            <Col md={LABEL_WIDTH}>Hard</Col>
+          </Row>
+          <br></br>
+          <Form.Label>Route Views</Form.Label>
+          <Row>
+            <Col md={LABEL_WIDTH}>Nature</Col>
+            <Col md={SLIDER_WIDTH}>
+              <Form.Control
+                type="range"
+                onChange={handleViewsChange}
+                value={views}
+              />
+            </Col>
+            <Col md={LABEL_WIDTH}>City</Col>
+          </Row>
+          <br></br>
+          <Form.Label>Route Traffic</Form.Label>
+          <Row>
+            <Col md={LABEL_WIDTH}>Light</Col>
+            <Col md={SLIDER_WIDTH}>
+              <Form.Control
+                type="range"
+                onChange={handleTrafficChange}
+                value={traffic}
+              />
+            </Col>
+            <Col md={LABEL_WIDTH}>Heavy</Col>
+          </Row>
+        </Form.Group>
+        <Button variant="secondary" type="submit" onClick={submitSearch}>
+          Find route
+        </Button>
+      </Form>
+
+
+                    {/* <CriteriaSliders submitHandler={handleChoseRoute}/> */}
                   </Tab>
                   <Tab eventKey="search" title="Search by Routes">
                     <br />
